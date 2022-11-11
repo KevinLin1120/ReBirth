107dc7a4c964757ffbf3d1b34e553abb4789a7f3
shiba_doggy <40301485+johnny900912@users.noreply.github.com>
2022-11-11 22:42:39 +0800

    TimeCountDown v1.0
    I make a  count down timer
    TimerCount.cs

--- rebirth/Assets/Scripts/TimerCount.cs
+++ rebirth/Assets/Scripts/TimerCount.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerCount : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject textDisplay;
+    public int secondLeft = 30;
+    public bool timeGo = false;
+
+
     void Start()
     {
-        
+        textDisplay.GetComponent<Text>().text = "00:" + secondLeft;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (timeGo == false&& secondLeft > 0)
+        {
+            StartCoroutine(TimeTake());
+        }
     }
+
+    IEnumerator TimeTake()
+    {
+        timeGo = true;
+        yield return new WaitForSeconds(1);
+        secondLeft -= 1;
+        if (secondLeft < 10)
+        {
+            textDisplay.GetComponent<Text>().text = "00:0" + secondLeft;
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "00:" + secondLeft;
+        }
+        timeGo = false;
+
+    }
+
 }
