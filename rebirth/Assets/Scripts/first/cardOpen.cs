a374b587050ad3464caddb0e06321c78da366988
howardchou <howard9107@gmail.com>
2022-11-25 17:20:17 +0800

    preliminary structure done
    version1
    can open card can close.
    no anim

---
+++ rebirth/Assets/Scripts/first/cardOpen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardOpen : MonoBehaviour
+{
+    public GameObject Panel;
+
+    // public void OpenPanel(){
+    //     if(Panel != null){
+    //         Panel.SetActive(true);
+    //     }
+    // }
+
+    private void OnMouseUpAsButton() {
+        Debug.Log("OnMouseUpAsButton");
+        if(Panel != null){
+            Panel.SetActive(true);
+        }
+    }
+}
