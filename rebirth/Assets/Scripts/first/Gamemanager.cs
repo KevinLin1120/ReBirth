dfae8a3f5acf0a7d2d542ab1f66c46dc7f7eeaf8
KevinLin1120 <kevinlin1120a@gmail.com>
2022-12-02 16:03:11 +0800

    Play video at the beginning

--- rebirth/Assets/Scripts/first/Gamemanager.cs
+++ rebirth/Assets/Scripts/first/Gamemanager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
 {
+    public GameObject blackPanel, video, skipBtn;
+
     public bool isDeath = false;
     public GameObject screenFlash;
     public blink blink;
@@ -11,13 +13,14 @@ public class Gamemanager : MonoBehaviour
     public reborn reborn;
     public Transform blink_top, blink_bottom;
 
-    
-
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible=true;
+        Cursor.visible = true;
         Cursor.lockState = 0;
+
+        // Delay 3s and indicate the btn
+        StartCoroutine(playVideo());
     }
 
     // Update is called once per frame
@@ -27,48 +30,52 @@ public class Gamemanager : MonoBehaviour
         if (isDeath)
         {
             StartCoroutine(death());
-            IEnumerator death(){
-                // Start the flash
-                screenFlash.SetActive(true);
-                for (int i = 0; i < 6; i++)
-                {
-                    Debug.Log("enter" + i); 
-                    yield return new WaitForSeconds(1.75f);
-                }
-                // Stop the flash
-                screenFlash.SetActive(false);
-
-                // Start blinking
-                blink.blink_eye(true);
-                // First time
-                yield return new WaitForSeconds(2.65f);
-                // Half of second time
-                yield return new WaitForSeconds(1.2f);
-                // Stop blinking
-                blink.blink_eye(false);
-
-                passOut.startPassOut(true);
-                yield return new WaitForSeconds(5f);
-                passOut.startPassOut(false);
-
-                blink_top.localPosition = new Vector3(0, 1.5f, 0.459999114f);
-                blink_bottom.localPosition = new Vector3(0, -1.5f, 0.459999114f);
-
-                reborn.startReborn(true);
-                yield return new WaitForSeconds(2f);
-                reborn.startReborn(false);
-
-            }
-            // Flash the player's screen
-            //screenFlash.flash(8);
-            // Blink
-            //blink.blink_eye(6);
-            // Reborn
-            //blink.openEye();
             isDeath = !isDeath;
-            
         }
     }
 
-    
+    IEnumerator death()
+    {
+        // Start the flash
+        screenFlash.SetActive(true);
+        for (int i = 0; i < 6; i++)
+        {
+            Debug.Log("enter" + i);
+            yield return new WaitForSeconds(1.75f);
+        }
+        // Stop the flash
+        screenFlash.SetActive(false);
+
+        // Start blinking
+        blink.blink_eye(true);
+        // First time
+        yield return new WaitForSeconds(2.65f);
+        // Half of second time
+        yield return new WaitForSeconds(1.2f);
+        // Stop blinking
+        blink.blink_eye(false);
+
+        passOut.startPassOut(true);
+        yield return new WaitForSeconds(5f);
+        passOut.startPassOut(false);
+
+        blink_top.localPosition = new Vector3(0, 1.5f, 0.459999114f);
+        blink_bottom.localPosition = new Vector3(0, -1.5f, 0.459999114f);
+
+        reborn.startReborn(true);
+        yield return new WaitForSeconds(2f);
+        reborn.startReborn(false);
+
+    }
+
+    IEnumerator playVideo()
+    {
+        blackPanel.SetActive(true);
+        // Delay 3s
+        yield return new WaitForSeconds(3);
+        blackPanel.SetActive(false);
+        video.SetActive(true);
+        yield return new WaitForSeconds(3);
+        skipBtn.SetActive(true);
+    }
 }
